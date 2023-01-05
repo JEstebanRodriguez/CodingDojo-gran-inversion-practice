c2450ed1c46703d768c9fd87786fa02ca5bfcced
XJDEVX <xjergx.dev@gmail.com>
2023-01-05 17:00:42 -0300

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import PersonCard from './components/PersonCard'
+const App = () => {
+	return (
+		<>
+			<PersonCard firstName='Jane' lastName='Doe' age={45} hairColor='black' />
+			<PersonCard
+				firstName='John'
+				lastName='Smith'
+				age={88}
+				hairColor='Brown'
+			/>
+			<PersonCard
+				firstName='Millard'
+				lastName='Fillmore'
+				age={50}
+				hairColor='Brown'
+			/>
+			<PersonCard
+				firstName='Maria'
+				lastName='Smith'
+				age={62}
+				hairColor='Brown'
+			/>
+		</>
+	)
+}
+export default App
